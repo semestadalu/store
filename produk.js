8fe0dbc033c892a669aba2726419ba33c250b9d0
semesta <semestadalu@gmail.com>
2026-09-23 22:11:14 +0700

    Add files via upload

---
+++ produk.js
@@ -0,0 +1,5 @@
+const dataProduk = [
+    { kategoriID: "web", labelKategori: "Web Tools", nama: "Sistem Manajemen UMKM Instan", deskripsi: "Web app berbasis HTML/JS siap pakai", harga: 100000, gambar: "web-tool.jpg" },
+    { kategoriID: "template", labelKategori: "Template", nama: "Dark Aesthetic Post Template", deskripsi: "Kumpulan 50 desain post IG", harga: 50000, gambar: "template.jpg" }
+    // NANTI DATA BARU COPAS KE SINI
+];

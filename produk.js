69bf1d2d18e2c028ee610d84823aeba33835d97e
semesta <semestadalu@gmail.com>
2026-09-24 17:27:28 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,10 +1,10 @@
 const dataProduk = [
     { kategoriID: "web", labelKategori: "Web Tools", nama: "Untitled", deskripsi: "Untitled", harga: 0, gambar: "web-tool.jpg" },
-    { kategoriID: "template", labelKategori: "Template", nama: "Untitled", deskripsi: "Untitled", harga: 0, gambar: "template.jpg" }
-    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Creator Blueprint", deskripsi: "Formula Copywriting FYP & Kit Bio-Link Cuan untuk narik perhatian audiens. Format PDF.", harga: 25000, gambar: "ebook1.jpg" }
-    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Kalkulator & Pelacak Keuangan", deskripsi: "Financial Survival Kit buat ngatur duit lu biar nggak bokek. Format PDF-include bonus CSV, File RAR", harga: 30000, gambar: "ebook2.jpg" }
-    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Microblog Mastery", deskripsi: "Template Konten Edukasi buat lu yang pemilik online shop, content creator, dan agensi kecil yang lagi butuh kepraktisan desain. Format PDF", harga: 25000, gambar: "ebook3.jpg" }
-    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Productivity Dashboard", deskripsi: "Dasbor Produktivitas Harian Berfungsi sebagai all-in-one planner digital yang interaktif dan menghemat waktu manajemen harian pembeli. Format PDF-include bonus CSV, File RAR", harga: 30000, gambar: "ebook4.jpg" }
+    { kategoriID: "template", labelKategori: "Template", nama: "Untitled", deskripsi: "Untitled", harga: 0, gambar: "template.jpg" },
+    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Creator Blueprint", deskripsi: "Formula Copywriting FYP & Kit Bio-Link Cuan untuk narik perhatian audiens. Format PDF.", harga: 25000, gambar: "ebook1.jpg" },
+    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Kalkulator & Pelacak Keuangan", deskripsi: "Financial Survival Kit buat ngatur duit lu biar nggak bokek. Format PDF-include bonus CSV, File RAR", harga: 30000, gambar: "ebook2.jpg" },
+    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Microblog Mastery", deskripsi: "Template Konten Edukasi buat lu yang pemilik online shop, content creator, dan agensi kecil yang lagi butuh kepraktisan desain. Format PDF", harga: 25000, gambar: "ebook3.jpg" },
+    { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Productivity Dashboard", deskripsi: "Dasbor Produktivitas Harian Berfungsi sebagai all-in-one planner digital yang interaktif dan menghemat waktu manajemen harian pembeli. Format PDF-include bonus CSV, File RAR", harga: 30000, gambar: "ebook4.jpg" },
     { kategoriID: "ebook", labelKategori: "E-Book & Hiburan", nama: "Bundel Persiapan Karir & Rekrutmen", deskripsi: "Template CV ATS biar lolos unutk fresh graduated atau profesianal yang pengen pindah kerja dan pengen hasil instan. Format PDF", harga: 25000, gambar: "ebook5.jpg" }
 
     // NANTI DATA BARU COPAS KE SINI
